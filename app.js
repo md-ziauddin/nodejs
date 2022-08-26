61c7e2bf7e250252f48cadff68b96ed9fec63a06
MD ZIAUDDIN <md.ziauddin02@gmail.com>
2022-08-26 13:57:35 +0530

    routes, views added

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-const express =require('express');
-const http = require('http')
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-     
-    res.send('<h1>Hello from express!!</h1>')
-    next()
-})
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.listen(3000, () => console.log('server started'))
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(3000);
